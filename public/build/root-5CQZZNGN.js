c6a23336011c4c0fc6c7fc6cd1612f3872ef333e
roopantj <ROOPANTJ7@GMAIL.COM>
2022-02-25 16:45:06 +0530

    Tailwind css

---
+++ public/build/root-5CQZZNGN.js
@@ -0,0 +1,38 @@
+import {
+  Links,
+  LiveReload,
+  Outlet
+} from "/build/_shared/chunk-XPZ6P5TY.js";
+import {
+  React,
+  init_react
+} from "/build/_shared/chunk-YZVLR6ER.js";
+
+// browser-route-module:C:\Users\91754\Desktop\Todo-Re\app\root.tsx?browser
+init_react();
+
+// app/root.tsx
+init_react();
+
+// app/styles/app.css
+var app_default = "/build/_assets/app-CQL5IY7A.css";
+
+// app/root.tsx
+var links = () => {
+  return [{ rel: "stylesheet", href: app_default }];
+};
+function App() {
+  return /* @__PURE__ */ React.createElement("html", {
+    lang: "en"
+  }, /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement("meta", {
+    charSet: "utf-8"
+  }), /* @__PURE__ */ React.createElement("meta", {
+    name: "viewport",
+    content: "width=device-width,initial-scale=1"
+  }), /* @__PURE__ */ React.createElement("title", null, "Remix JS new"), /* @__PURE__ */ React.createElement(Links, null)), /* @__PURE__ */ React.createElement("body", null, /* @__PURE__ */ React.createElement(Outlet, null), /* @__PURE__ */ React.createElement(LiveReload, null)));
+}
+export {
+  App as default,
+  links
+};
+//# sourceMappingURL=/build/root-5CQZZNGN.js.map
---
+++ public/build/routes/index-5ZP2U7RZ.js
@@ -0,0 +1,17 @@
+import {
+  React,
+  init_react
+} from "/build/_shared/chunk-YZVLR6ER.js";
+
+// browser-route-module:C:\Users\91754\Desktop\Todo-Re\app\routes\index.tsx?browser
+init_react();
+
+// app/routes/index.tsx
+init_react();
+function HomePage() {
+  return /* @__PURE__ */ React.createElement("h1", null, "Homepage");
+}
+export {
+  HomePage as default
+};
+//# sourceMappingURL=/build/routes/index-5ZP2U7RZ.js.map
